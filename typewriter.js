666f2ce535b18d278448d59f1be1b0540643c86a
Jonas Friis Kjær <jona74c2@stud.kea.dk>
2020-02-20 11:08:03 +0100

    typewriterV2 is now ready

--- typewriter.js
+++ typewriter.js
@@ -1,9 +1,8 @@
 "use strict";
 
 window.addEventListener("DOMContentLoaded", start);
-let typewrittenElement = document.querySelector(".typewritten");
-let typewrittenStr = document.querySelector(".typewritten").textContent;
-let typewrittenSubStr;
+//let typewrittenElement = document.querySelector(".typewritten");
+//let typewrittenStr = document.querySelector(".typewritten").textContent;
 let letterCounter = 0;
 
 let enableSoundBool = false;
@@ -18,56 +17,68 @@ let typeLetterSound2 = document.querySelector("#typekey2").cloneNode(true);
 let typeLetterSound2b = document.querySelector("#typekey2");
 
 function start() {
-  console.log(typewrittenStr);
+  const element = document.querySelector(".typewritten");
+  typeWriter(element, done);
+}
+
+function done() {
+  console.log("Done!");
+}
+
+function typeWriter(element, callback) {
+  let typewrittenSubStr;
   //make typewritten string empty
   document.addEventListener("click", enableSound);
-  document.querySelector("#sounds").textContent =
-    "Click anywhere to enable sound";
-  typewrittenElement.textContent = "";
+  document.querySelector("#sounds").textContent = "Click anywhere to enable sound";
+  let typewrittenStr = element.textContent;
+  element.textContent = "";
+  console.log(letterCounter);
+  console.log(typewrittenStr.length);
   showNextLetter();
-}
+  function showNextLetter() {
+    typewrittenSubStr = typewrittenStr.substring(0, letterCounter + 1);
+    playSound(typewrittenSubStr);
+    console.log(typewrittenSubStr);
 
-function showNextLetter() {
-  typewrittenSubStr = typewrittenStr.substring(0, letterCounter + 1);
-  playSound();
-  console.log(typewrittenSubStr);
+    element.textContent = typewrittenSubStr;
 
-  typewrittenElement.textContent = typewrittenSubStr;
-
-  letterCounter++;
-
-  if (letterCounter < typewrittenStr.length) {
-    const offset = Math.random() * 100 + 50;
-    setTimeout(showNextLetter, offset);
-  }
-}
-
-function enableSound() {
-  enableSoundBool = true;
-}
-
-function playSound() {
-  //add sounds to string
-  if (enableSoundBool) {
-    if (typewrittenSubStr.charAt(typewrittenSubStr.length - 1) == " ") {
-      typeSpaceSound.play();
-      console.log("We are playing sound!");
-    } else if (Math.random() > 0.5) {
-      if (sound1Playing) {
-        typeLetterSound1b.play();
-        sound1Playing = false;
-      } else {
-        typeLetterSound1.play();
-        sound1Playing = true;
-      }
+    letterCounter++;
+    if (letterCounter < typewrittenStr.length) {
+      const offset = Math.random() * 100 + 50;
+      setTimeout(showNextLetter, offset);
     } else {
-      if (sound2Playing) {
-        typeLetterSound2.play();
-        sound2Playing = false;
+      callback();
+    }
+  }
+  function enableSound() {
+    enableSoundBool = true;
+  }
+
+  function playSound(typewrittenSubStr) {
+    //add sounds to string
+    if (enableSoundBool) {
+      //console.log("We are playing sound!");
+      if (typewrittenSubStr.charAt(typewrittenSubStr.length - 1) == " ") {
+        typeSpaceSound.play();
+      } else if (Math.random() > 0.5) {
+        if (sound1Playing) {
+          typeLetterSound1b.play();
+          sound1Playing = false;
+        } else {
+          typeLetterSound1.play();
+          sound1Playing = true;
+        }
       } else {
-        typeLetterSound2b.play();
-        sound2Playing = true;
+        if (sound2Playing) {
+          typeLetterSound2.play();
+          sound2Playing = false;
+        } else {
+          typeLetterSound2b.play();
+          sound2Playing = true;
+        }
       }
     }
   }
 }
+
+//Link to site through github: https://jona74c2.github.io/typewriter/typewriter.html
